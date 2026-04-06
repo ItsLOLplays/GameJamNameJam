b90e85ec639f4c5458862a240893ec1f493bbd87
Jonas Van Houdt Sperber <jonasvanhoudtsperber@gmail.com>
2026-04-06 17:39:07 +0200

    add files from unversioned files

---
+++ Assets/Fractures/Fracture.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Fracture : MonoBehaviour
+{
+    public GameObject fractureObject;
+    public GameObject originalObject;
+    public float explosionMinForce = 5f;
+    public float explosionMaxForce = 100f;
+    public float explosionForceRadius = 10f;
+    public float fragScaleFactor = 1f;
+    public AudioSource breakSound;
+    public AudioSource debrisSound;
+
+    private GameObject fractObject;
+    private bool hasExploded;
+    private bool hasPlayedBreak;
+    private bool hasPlayedDebris;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        if (originalObject != null && hasExploded == false)
+        {
+            originalObject.SetActive(false);
+            breakSound.Play();
+            debrisSound.PlayDelayed(breakSound.clip.length / 2f);
+
+            if (fractureObject != null)
+            {
+                Vector3 position = originalObject.transform.position;
+                position.x += 6.75f;
+                position.z -= 5.75f;
+                
+                fractObject = Instantiate(fractureObject) as GameObject;
+                fractObject.transform.position = position;
+
+                foreach (Transform t in fractObject.transform)
+                {
+                    var rb = t.GetComponent<Rigidbody>();
+                    
+                    if (rb != null)
+                    {
+                        Random.InitState(DateTime.Now.Millisecond);
+                        rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce),
+                            originalObject.transform.position, explosionForceRadius);
+                    }
+
+                    StartCoroutine(Shrink(t, 6f));
+                }
+
+                StopCoroutine(nameof(Shrink));
+                Destroy(fractObject, 15f);
+            }
+        }
+    }
+
+    IEnumerator Shrink(Transform t, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Vector3 newScale = t.localScale;
+
+        while (newScale.x >= 0)
+        {
+            newScale -= new Vector3(fragScaleFactor, fragScaleFactor, fragScaleFactor);
+
+            t.localScale = newScale;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
